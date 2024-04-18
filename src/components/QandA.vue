64ec9499e7b415a76510bfe8bd450fc21b7cdc39
LSK <seulki-2>
2024-04-18 21:30:49 +0900

    Search 및 Footer

---
+++ src/components/QandA.vue
@@ -0,0 +1,180 @@
+<template>
+    <h1>공지사항 </h1>
+    <div class="row">
+        <div class="card card-margin search-form">
+            <div class="card-body p-0">
+                <form id="search-form">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="row no-gutters">
+                                <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                    <label for="search-type" hidden>검색 유형</label>
+                                    <select class="form-control" id="search-type" name="searchType">
+                                        <option>제목</option>
+                                        <option>본문</option>
+                                        <option>id</option>
+                                        <option>닉네임</option>
+                                        <option>해시태그</option>
+                                    </select>
+                                </div>
+                                <div class="col-lg-8 col-md-6 col-sm-12 p-0">
+                                    <label for="search-value" hidden>검색어</label>
+                                    <input type="text" placeholder="검색어..." class="form-control" id="search-value"
+                                           name="searchValue">
+                                </div>
+                                <div class="col-lg-1 col-md-3 col-sm-12 p-0">
+                                    <button type="submit" class="btn btn-base">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                             stroke-linecap="round" stroke-linejoin="round"
+                                             class="feather feather-search">
+                                            <circle cx="11" cy="11" r="8"></circle>
+                                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <table class="table" id="article-table">
+            <thead>
+            <tr>
+                <th class="title col-6"><a>제목</a></th>
+                <th class="hashtag col-2"><a>해시태그</a></th>
+                <th class="user-id"><a>작성자</a></th>
+                <th class="created-at"><a>작성일</a></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td class="title"><a>첫글</a></td>
+                <td class="hashtag">#java</td>
+                <td class="user-id">Uno</td>
+                <td class="created-at">
+                    <time>2022-01-01</time>
+                </td>
+            </tr>
+            <tr>
+                <td>두번째글</td>
+                <td>#spring</td>
+                <td>Uno</td>
+                <td>
+                    <time>2022-01-02</time>
+                </td>
+            </tr>
+            <tr>
+                <td>세번째글</td>
+                <td>#java</td>
+                <td>Uno</td>
+                <td>
+                    <time>2022-01-03</time>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="row">
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <a class="btn btn-primary me-md-2" role="button" id="write-article">글쓰기</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <nav id="pagination" aria-label="Page navigation">
+            <ul class="pagination justify-content-center">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'QandA'
+    
+  }
+  </script>
+  
+  <style>
+        .search-form {
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 1rem;
+}
+
+.search-form input {
+  height: 100%;
+  background: transparent;
+  border: 0;
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  height: 100%;
+  font-size: 1rem;
+}
+
+.search-form select {
+  background: transparent;
+  border: 0;
+  padding: 1rem;
+  height: 100%;
+  font-size: 1rem;
+}
+
+.search-form select:focus {
+  border: 0;
+}
+
+.search-form button {
+  height: 100%;
+  width: 100%;
+  font-size: 1rem;
+}
+
+.search-form button svg {
+  width: 24px;
+  height: 24px;
+}
+
+.card-margin {
+  margin-bottom: 1.875rem;
+}
+
+@media (min-width: 992px) {
+  .col-lg-2 {
+    flex: 0 0 16.66667%;
+    max-width: 16.66667%;
+  }
+}
+
+.card {
+  border: 0;
+  box-shadow: 0px 0px 10px 0px rgba(82, 63, 105, 0.1);
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(82, 63, 105, 0.1);
+  -moz-box-shadow: 0px 0px 10px 0px rgba(82, 63, 105, 0.1);
+  -ms-box-shadow: 0px 0px 10px 0px rgba(82, 63, 105, 0.1);
+}
+
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #ffffff;
+  background-clip: border-box;
+  border: 1px solid #e6e4e9;
+  border-radius: 8px;
+}
+
+    </style>
+  
---
+++ src/components/TripSearch.vue
@@ -0,0 +1,95 @@
+<template><br><br><br><br><br><br>
+  <h1>어디로 떠나볼까요?</h1><br><br><br><br>
+  <nav class="navbar navbar-light bg-white">
+  <div class="container-fluid justify-content-center">
+    <form class="d-flex justify-content-center">
+      <input class="form-control form-control-lg me-2" type="search" placeholder="가고 싶은 지역을 찾아보세요" aria-label="Search">
+      <button class="btn btn-outline-success btn-lg" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+  <br><br><br><br><br>
+  <div class="row">
+    <div class="col-md-4">
+      <img :src="seoul" class="img-fluid rounded zoom fixed-size" alt="Seoul"><br>
+      <span class="text">서 울<br>SEOUL</span>
+    </div>
+    <div class="col-md-4">
+      <img :src="busan" class="img-fluid rounded zoom fixed-size" alt="Busan"><br>
+      <span class="text">부 산<br>BUSAN</span>
+    </div>
+    <div class="col-md-4">
+      <img :src="jeju" class="img-fluid rounded zoom fixed-size" alt="Jeju"><br>
+      <span class="text">제 주<br>JEJU</span> <br><br><br><br>
+    </div>
+    <div class="col-md-4">
+      <img :src="gangwon" class="img-fluid rounded zoom fixed-size" alt="Gangwon"><br>
+      <span class="text">강 원<br>GAMGWON</span>
+    </div>
+    <div class="col-md-4">
+      <img :src="daegu" class="img-fluid rounded zoom fixed-size" alt="Daegu"><br>
+      <span class="text">대 구<br>DAEGU</span>
+    </div>
+    <div class="col-md-4">
+      <img :src="gyeongju" class="img-fluid rounded zoom fixed-size" alt="Gyeongju"><br>
+      <span class="text">경 주<br>GYEONGJU</span>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'TripSearch',
+  data(){
+    return{
+      busan: require('@/assets/main_busan.png'),
+      jeju: require('@/assets/main_jeju.png'), // 경로가 잘못 지정되어 있었으므로 수정합니다.
+      seoul: require('@/assets/main_seoul.jpeg'),
+      gangwon: require('@/assets/main_gangwon.jpg'),
+      daegu: require('@/assets/main_daegu.jpg'),
+      gyeongju: require('@/assets/main_gyeongju.png'),
+    }
+  }
+}
+</script>
+
+<style>
+ .navbar {
+    display: flex;
+    justify-content: center; /* Flexbox를 사용한 가운데 정렬 */
+    width: 100%; /* 필요에 따라 조정 */
+  }
+.form-control::placeholder {
+    font-size: 16px;
+}
+.text {
+    font-size: 20px;
+}
+.form-control {
+  width: 300px;
+}
+.center-block {
+  margin: 0 auto;
+  width: 50%;
+}
+
+/* 이미지 호버 효과 */
+.zoom {
+  transition: transform .2s; /* 애니메이션 속도 */
+  width: 80%; /* 부모 요소에 꽉 차게 */
+  height: auto; /* 이미지 원본 비율 유지 */
+}
+
+.zoom:hover {
+  transform: scale(1.1); /* 10% 확대 */
+}
+.fixed-size {
+  width: 400px !important;
+  height: 500px !important;
+  object-fit: cover !important;/* 이미지 비율을 유지하면서 요소 크기에 맞게 조정 */
+}
+
+
+</style>
